884eafc6a9bb691ec9a4559e699638dc6fc2ca4c
cesarus777 <47499462+cesarus777@users.noreply.github.com>
2019-07-11 12:38:55 +0300

    Add files via upload

--- judge/matrix_pow/matrix.c
+++ judge/matrix_pow/matrix.c
@@ -1,63 +1,156 @@
-#include "addition.h"
+#include "assert.h"
+#include "math.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
-/* In addition.c you can configure this programm, depending on your needs */
-
-struct matrix_t matrix_mult(struct matrix_t a, struct matrix_t b)
+struct matrix_t
 {
-    assert(a.size == b.size);
-    struct matrix_t res;
-    res.size = a.size;
-    res.elems = (double *) calloc(res.size * res.size, sizeof(double));
-    assert(res.elems != NULL && a.elems != NULL && b.elems != NULL);
-    for(int i = 0; i < res.size; ++i)
-        for(int j = 0; j < res.size; ++j)
-            for(int k = 0; k < res.size; ++k)
-                res.elems[i * res.size + j] += a.elems[i * res.size + k] * b.elems[k * res.size + j];
+    int size;
+    double *elems;
+};
+
+void pm(struct matrix_t m)
+{
+    for(int i = 0; i < m.size; ++i)
+    {
+        for(int j = 0; j < m.size; ++j)
+            printf(" %lf", m.elems[i * m.size + j]);
+        printf("\n");
+    }
+}
+
+int comp(const void *a, const void *b)
+{
+    double x = *(double *) a;
+    double y = *(double *) b;
+    if(fabs(x - y) < (0.00001))
+        return 0;
+    if(x > y)
+        return 1;
+    return -1;
+}
+
+double mtx_sum(struct matrix_t mtx)
+{
+    qsort(mtx.elems, mtx.size * mtx.size, sizeof(double), &comp);
+    double res = 0;
+    for(int i = 0; i < mtx.size; ++i)
+        for(int j = 0; j < mtx.size; ++j)
+            res += mtx.elems[i * mtx.size + j];
     return res;
 }
 
-struct matrix_t matrix_pown(struct matrix_t mtx, int p)
+void matrix_mult(struct matrix_t a, struct matrix_t b, struct matrix_t c)
 {
-    struct matrix_t prod;
-    prod.size = mtx.size;
-    prod.elems = (double *) calloc(prod.size * prod.size, sizeof(double));
-    
+    assert((a.size == b.size) && (b.size == c.size));
+    assert(a.elems != NULL && b.elems != NULL && c.elems != NULL);
+    int size = a.size;
+    for(int i = 0; i < size; ++i)
+        for(int j = 0; j < size; ++j)
+        {
+            a.elems[i * size + j] = 0;
+            for(int k = 0; k < size; ++k)
+                a.elems[i * size + j] += b.elems[i * size + k] * c.elems[j * size + k];
+        }
+}
+
+void matrix_transposition(struct matrix_t a, struct matrix_t a_t)
+{
+    assert(a.size == a_t.size);
+    assert((a.elems != NULL) && (a_t.elems != NULL));
+    for(int i = 0; i < a.size; ++i)
+    {
+        for(int j = 0; j < a.size; ++j)
+        {
+            a_t.elems[j * a.size + i] = a.elems[i * a.size + j];
+        }
+    }
+}
+
+void matrix_pown(struct matrix_t in_mtx, struct matrix_t out_mtx, struct matrix_t tmp_mtx, int p)
+{
+    assert((in_mtx.size == out_mtx.size) && (out_mtx.size == tmp_mtx.size));
+    assert((in_mtx.elems != NULL) && (out_mtx.elems != NULL) && (tmp_mtx.elems != NULL));
+    struct matrix_t mid_res;
+    int size = in_mtx.size;
+    mid_res.size = size;
+    mid_res.elems = (double *) calloc(size * size, sizeof(double));
+   
     //at start prod must be unit matrix
-    for(int i = 0; i < prod.size; ++i)
-        prod.elems[i * prod.size + i] = 1;
+    for(int i = 0; i < size; ++i)
+        out_mtx.elems[i * size + i] = 1;
 
     while(p > 0)
     {
         if((p % 2) == 1)
         {
-            prod = matrix_mult(prod, mtx);
+            matrix_transposition(in_mtx, tmp_mtx);
+            matrix_mult(mid_res, out_mtx, tmp_mtx);
+            memcpy(out_mtx.elems, mid_res.elems, size * size * sizeof(double));
             p -= 1;
         } else {
-            mtx = matrix_mult(mtx, mtx);
+            matrix_transposition(out_mtx, tmp_mtx);
+            matrix_mult(mid_res, out_mtx, tmp_mtx);
+            memcpy(out_mtx.elems, mid_res.elems, size * size * sizeof(double));
             p /= 2;
         }
     }
 
-    return prod;
+    free(mid_res.elems);
+}
+
+void fill_input_matrix(struct matrix_t *input_matrix, int *p)
+{
+    int size; 
+    int check = scanf("%d", p);
+    assert(check == 1);
+    check = scanf("%d", &size);
+    assert(check == 1);
+    input_matrix->size = size;
+    input_matrix->elems = calloc(size * size, sizeof(double));
+    assert(input_matrix->elems != NULL);
+    double x;
+    for(int i = 0; i < size; ++i)
+    {
+        for(int j = 0; j < size; ++j)
+        {
+            check = scanf("%lf", &x);
+            assert(check == 1);
+            input_matrix->elems[i * size + j] = x;
+        }
+    }
+}
+
+void calc_n_print_res(struct matrix_t mtx)
+{
+
+    double res;
+
+    //pm(mtx);
+ 
+    res = mtx_sum(mtx);
+
+    printf("%lf\n", res);
+
 }
 
 int
 main()
 {
     int size, p;
-    struct matrix_t input_matrix, mul_matrix;
+    struct matrix_t input_matrix, mul_matrix, trans;
 
     fill_input_matrix(&input_matrix, &p);
 
     size = input_matrix.size;
     mul_matrix.size = size;
+    trans.size = size;
     mul_matrix.elems = calloc(size * size, sizeof(double));
-    assert(mul_matrix.elems != NULL);
-    mul_matrix = matrix_pown(input_matrix, p);
-
-#ifdef DEBUG_PRINT
-    print_matrix(mul_matrix);
-#endif
+    trans.elems = calloc(size * size, sizeof(double));
+    assert((mul_matrix.elems != NULL) && (trans.elems != NULL));
+    matrix_pown(input_matrix, mul_matrix, trans, p);
+    free(trans.elems);
 
     calc_n_print_res(mul_matrix);
     
